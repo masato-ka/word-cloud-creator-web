2aa081820316c1f4fbf1e76ce021b153f30ed3a6
masato-ka <jp6uzv@gmail.com>
2019-02-25 23:37:19 +0900

    Add test code as get current directory.

--- src/main/java/ka/masato/blog/wordcloud/WordCloudApplication.java
+++ src/main/java/ka/masato/blog/wordcloud/WordCloudApplication.java
@@ -12,10 +12,10 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.File;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 @SpringBootApplication
 public class WordCloudApplication implements CommandLineRunner {
@@ -48,9 +48,9 @@ public class WordCloudApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        String cd = new File(".").getAbsoluteFile().getParent();
-        logger.info(cd);
-//        String fontConfig = System.getProperty("java.home")
+        Properties hoge = System.getProperties();
+        hoge.keySet().stream().forEach(System.out::println);
+        //        String fontConfig = System.getProperty("java.home")
 //                + File.separator + "lib"
 //                + File.separator + "fontconfig.Prodimage.properties";
 //        logger.info(fontConfig);
